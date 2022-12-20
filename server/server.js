a0f992b1dfc2f909b08d77cbe7517521908262bf
jmizis <107073756+jmizis@users.noreply.github.com>
2022-12-20 15:45:40 -0500

    server/client app

--- server/server.js
+++ server/server.js
@@ -3,8 +3,26 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const {authMiddleware} = require('./utils/auth')
+// Import the ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,7 +33,15 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
-
+const startApolloServer = async (typeDefs,resolvers) => {
+await server.start()
+server.applyMiddleware({app})
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+}
+
+
+
+startApolloServer(typeDefs, resolvers);
